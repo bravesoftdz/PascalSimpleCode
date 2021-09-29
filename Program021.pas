8959730e695767b8e2728e483ff34fd638790203
LetNull19A <alexvolkovdd@gmail.com>
2021-09-30 01:44:50 +0300

    Хей как дела?

---
+++ Program021.pas
@@ -0,0 +1,29 @@
+﻿Program prog;
+  var a, b, c, _middle : integer;
+  var message : string;
+Begin
+  Writeln('Введите число a');
+  Readln(a);
+  Writeln('Введите число b');
+  Readln(b);
+  Writeln('Введите число c');
+  Readln(c);
+  
+  _middle:= -1;
+  
+  if (((a < b) OR (a < c)) AND ((a > b) OR (a > c)) AND (_middle = -1)) then
+    _middle:= a;
+  
+  if (((b < a) OR (b < c)) AND ((b > a) OR (b > c)) AND (_middle = -1)) then
+    _middle:= b;
+  
+  if (((c < a) OR (c < b)) AND ((c > a) OR (c > b)) AND (_middle = -1)) then
+    _middle:= c;
+  
+  if (NOT (_middle = -1)) then 
+    message:= 'Стреднее число это: ' + _middle
+  else if (_middle = -1) then
+    message:= 'В ведёных числах есть совпадающие!';
+  
+  Writeln(message);
+End.
---
+++ Program022.pas
@@ -0,0 +1,18 @@
+﻿Program prog;
+  var mouthNumber : integer;
+  var message : string;
+Begin
+  Writeln('Введите число месяца');
+  Readln(mouthNumber);
+  
+  if ((mouthNumber < 13) AND (mouthNumber > 0)) then
+    begin
+      case mouthNumber of 
+      1,3,5,7,8,10,12 : message:= 'В месяце 31 день.'; 
+      4,6,9,11 : message:= 'В месяце 30 дней.'; 
+      2 : message:= 'В месяце 28 дней.'; 
+      end;
+    end else message:= 'Вы ввели несуществующий месяц!';
+  
+  Writeln(message);
+End.
